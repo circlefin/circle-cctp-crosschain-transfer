34133dcd3c3c2d51ce7cec3b73ac840b42852d84
Chen Hui Jing <huijing.chen@circle.com>
2025-10-27 04:12:59 +0000

    chore: migrate linter, fix linter issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,10 +16,17 @@
  * limitations under the License.
  */
 
-const config = {
-  plugins: {
-    '@tailwindcss/postcss': {},
-  },
-};
+import { defineConfig, globalIgnores } from "eslint/config";
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-export default config;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig([
+    globalIgnores(["**/node_modules", "**/.next", "**/dist", "**/build", "**/out"]),
+    {
+        extends: [...nextCoreWebVitals],
+    },
+]);
